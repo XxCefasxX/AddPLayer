3d6fff631d38fac65c7a79ffd100b4bb3bc9721b
Pedro Salazar <salazar9_5@hotmail.com>
2026-01-03 16:18:43 -0700

    ui y sdk 24

--- app/src/main/java/com/cefasbysoftps/addplayer/views/history/HistoryScreen.kt
+++ app/src/main/java/com/cefasbysoftps/addplayer/views/history/HistoryScreen.kt
@@ -1,12 +1,25 @@
 import android.app.Application
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -15,7 +28,7 @@ import androidx.navigation.NavController
 @Composable
 fun HistoryScreen(
     navController: NavController
-){
+) {
     val context = LocalContext.current
 
     val sessionDataStore = remember {
@@ -40,12 +53,54 @@ fun HistoryScreen(
                 }
             }
         )
-        Button(modifier = Modifier.padding(top = 16.dp), onClick = {
-            var tiempo=playerViewModel.accumulatedTimeMs / 1000 / 60
-            val date = playerViewModel.currentDate
-            viewModel.sendReport(id, date, tiempo.toInt())
-        }) {
-            Text("Enviar")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(6.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Tiempo reproducido hoy",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = "${playerViewModel.accumulatedTimeMs / 1000 / 60} min",
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp),
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.DarkGray, // Color de fondo
+                        contentColor = Color.White  // Color del texto/icono
+                    ),
+                    onClick = {
+                        var tiempo = playerViewModel.accumulatedTimeMs / 1000 / 60
+                        val date = playerViewModel.currentDate
+                        viewModel.sendReport(id, date, tiempo.toInt())
+                    }) {
+                    Text("Enviar",
+                        style = MaterialTheme.typography.titleLarge)
+                }
+            }
+
+
         }
+
     }
 }
